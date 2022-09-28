56d598846e12dd84273c8c3975fe22df809c51e9
Daniel Cazzulino <daniel@cazzulino.com>
2022-09-28 16:25:27 -0300

    Add support for MEF attributes
    Fixes #17

--- src/DependencyInjection.Attributed.Tests/GenerationTests.cs
+++ src/DependencyInjection.Attributed.Tests/GenerationTests.cs
@@ -4,7 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Tests;
+namespace Tests.DependencyInjection;
 
 public record GenerationTests(ITestOutputHelper Output)
 {
